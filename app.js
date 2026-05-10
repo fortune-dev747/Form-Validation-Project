2f958193314a608f91160411b9dc020cb003cdef
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-05-10 04:14:42 +0100

    made adjustments to both JS and CSS

--- app.js
+++ app.js
@@ -1,47 +1,48 @@
+// get the form elements from the html by their id 
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// add an event listener to the form sign up button
+form.addEventListener('submit', event => {
+    event.preventDefault();
 
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  validateInputs();
+    verifyInputs();
 });
-
+// set the functionality to the errors
 const setError = (element, message) => {
     const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+    const errorDisplay = inputControl.querySelector('.error')
 
     errorDisplay.innerText = message;
     inputControl.classList.add('error');
     inputControl.classList.remove('success');
 }
-
-const setSuccess = element => {
+// set the functionality to successful followed instructions
+  const setSuccess = element => {
     const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+    const errorDisplay = inputControl.querySelector('.error')
 
     errorDisplay.innerText = '';
     inputControl.classList.add('success');
     inputControl.classList.remove('error');
-    
-
 }
-
+// checks validity of the email
 const isValidEmail = email => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+// assign the function you are calling in the event listener for the button
 
-const validateInputs = () => {
+
+const verifyInputs = () => {
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
+    const password2Value = password.value.trim();
+
 
     if (usernameValue === '') {
         setError(username, 'Username is required');
@@ -50,26 +51,26 @@ const validateInputs = () => {
     }
 
     if (emailValue === '') {
-        setError(email, 'Email is required');
+        setError(email, 'Please enter an email address');
     } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Provide a valid email address')
+        setError(email, 'Provide a valid email address!');
     } else {
         setSuccess(email);
     }
 
     if (passwordValue === '') {
-        setError(password, 'Password is required');
+        setError(password, 'Enter your password');
     } else if (passwordValue.length < 8) {
-        setError(password, 'Password must be at least 8 characters')
-    }  else {
+        setError(password, 'Password must contain at least 8 or more characters');
+    } else {
         setSuccess(password);
     }
 
     if (password2Value === '') {
         setError(password2, 'Please confirm your password');
     } else if (password2Value !== passwordValue) {
-        setError(password2, 'Passwords does not match');
-    }  else {
+        setError(password2, 'Password does not match');
+    } else {
         setSuccess(password2);
     }
 };
