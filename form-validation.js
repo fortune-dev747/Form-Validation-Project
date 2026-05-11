756052f254d22b07042bfb6d423b5168f00d8cad
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-05-11 01:09:04 +0100

    fixed the timing of the modal popup

--- form-validation.js
+++ form-validation.js
@@ -5,11 +5,13 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+
 // add an event listener to the form sign up button
 form.addEventListener('submit', event => {
     event.preventDefault();
 
     verifyInputs();
+    
 });
 // set the functionality to the errors
 const setError = (element, message) => {
@@ -36,18 +38,19 @@ const isValidEmail = email => {
 }
 // assign the function you are calling in the event listener for the button
 
-
 const verifyInputs = () => {
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
-    const password2Value = password.value.trim();
+    const password2Value = password2.value.trim();
 
 
     if (usernameValue === '') {
         setError(username, 'Username is required');
     } else {
         setSuccess(username);
+        // once the info has the success class then open the Modal
+        openPopup();
     }
 
     if (emailValue === '') {
@@ -56,6 +59,8 @@ const verifyInputs = () => {
         setError(email, 'Provide a valid email address!');
     } else {
         setSuccess(email);
+        // once the info has the success class then open the Modal
+        openPopup();
     }
 
     if (passwordValue === '') {
@@ -64,6 +69,8 @@ const verifyInputs = () => {
         setError(password, 'Password must contain at least 8 or more characters');
     } else {
         setSuccess(password);
+        // once the info has the success class then open the Modal
+        openPopup();
     }
 
     if (password2Value === '') {
@@ -72,7 +79,12 @@ const verifyInputs = () => {
         setError(password2, 'Password does not match');
     } else {
         setSuccess(password2);
+        // once the info has the success class then open the Modal
+        openPopup();
     }
 };
+
+
+
 
 
--- modal-popup.js
+++ modal-popup.js
@@ -1,9 +1,20 @@
 // Get the modal element from the html
 const popup = document.getElementById('myModal');
+const toggle = document.getElementById('toggleBtn');
+
+
+
+ if (toggle) {
+    toggle.addEventListener('click', event =>  {
+      if (toggle) {
+         popup.style.display = "none";
+      }
+    });
+};
 
 // Function to open Modal
-function openPopup(e) {
-    popup.style.display = "block";
+function openPopup() {
+        popup.style.display = "block";
 };
 
 // Function to close Modal
